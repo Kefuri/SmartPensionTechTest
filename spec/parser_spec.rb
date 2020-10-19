56a02b66f39dbd9e92b570a76dbe1c36a7c8fcb4
Haydon Houghton <haydonhoughton@outlook.com>
2020-10-19 12:37:29 +0100

    ammend test to read std_out

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,9 +3,9 @@ require 'parser'
 describe LogParser do
   context "print_log" do
     it "should print out the log given to the script" do
-      log = ARGV
+      log = 'log'
       parser = LogParser.new(log)
-      expect(parser.print_log).to eq(log + '\n')
+      expect { parser.print_log }.to output(log + "\n").to_stdout
     end
   end
 end
