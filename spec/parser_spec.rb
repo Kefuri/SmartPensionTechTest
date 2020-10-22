9ef13b270f71772f87732ab67664798498344cb2
Haydon Houghton <haydonhoughton@outlook.com>
2020-10-22 15:57:32 +0100

    add test for unique domain visits

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -157,4 +157,14 @@ describe LogParser do
       expect { parser.printsort_domains_by_visits(hash) }.to output("/domainnametwo 3 visits\n/domainname 1 visit\n").to_stdout
     end
   end
+
+  context "#printsort_unique_domains" do
+    it "should print each domain in the hash" do
+      logfile=""
+      parser = LogParser.new(logfile)
+      visits = ["/domainname1 192.111.111.111", "/domainname3 192.111.111.111", "/domainname1 192.111.111.112"]
+      hash = parser.create_domain_hash(visits)
+      expect{ parser.printsort_unique_domains(hash) }.to output("/domainname1 2 unique visits\n/domainname3 1 unique visit").to_stdout
+    end
+  end
 end
